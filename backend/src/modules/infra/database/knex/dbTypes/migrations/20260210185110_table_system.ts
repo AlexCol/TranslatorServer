ccba2cf50256686b143be207d931f300b5cca65e
AlexCol <eu_axil@yahoo.com.br>
2026-02-10 16:58:17 -0300

    refatorando

--- backend/src/modules/infra/database/knex/dbTypes/migrations/20260210185110_table_system.ts
+++ backend/src/modules/infra/database/knex/dbTypes/migrations/20260210185110_table_system.ts
@@ -1,18 +1,17 @@
 import type { Knex } from 'knex';
-import { seed } from '../seeds/001_environment';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('environments', (table) => {
-    table.string('name').primary(); // 'dev', 'prod', 'staging'
+  await knex.schema.createTable('systems', (table) => {
+    table.increments('id').primary();
+    table.string('name').notNullable(); // 'backend', 'web'
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
 
+    table.unique(['name']);
     table.index(['name']);
   });
-
-  await seed(knex);
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists('environments');
+  await knex.schema.dropTableIfExists('systems');
 }
--- backend/src/modules/infra/database/knex/dbTypes/migrations/20260210185112_table_languages.ts
+++ backend/src/modules/infra/database/knex/dbTypes/migrations/20260210185112_table_languages.ts
@@ -2,10 +2,16 @@ import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('languages', (table) => {
-    table.string('code').primary(); // 'pt-BR', 'en-US'
+    table.increments('id').primary();
+    table.integer('environment_id').unsigned().notNullable();
+    table.string('code').notNullable().unique(); // 'pt-BR', 'en-US'
     table.boolean('is_base').notNullable().defaultTo(false); // Marca linguagem base
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
+
+    table.foreign('environment_id').references('id').inTable('environments').onDelete('CASCADE');
+
+    table.unique(['environment_id', 'code']); // Garante que não haja linguagens duplicadas para o mesmo ambiente
 
     table.index(['code']);
     table.index(['is_base']);
